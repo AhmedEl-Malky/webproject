76cb08d7f89a94eec101124e5ad8542cca50fbe1
ahmed elmalky <ahmedelmalky35@gmail.com>
2023-04-17 14:50:38 +0200

    new tips page

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// ============================= LOGIN POPUP =========================
 document.querySelector("#login-button").addEventListener("click", function () {
   document.querySelector(".popout").classList.add("active");
 });
@@ -6,3 +7,8 @@ document
   .addEventListener("click", function () {
     document.querySelector(".popout").classList.remove("active");
   });
+// ============================= ACCORDION =========================
+document.querySelector(".title").addEventListener("click", function () {
+  document.querySelector(".acc-content").classList.add("active");
+  document.querySelector(".title").classList.add("active");
+});
