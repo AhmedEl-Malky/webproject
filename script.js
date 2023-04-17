c44561cd907092e79f93b5b65f60081be8fc161e
ahmed elmalky <ahmedelmalky35@gmail.com>
2023-04-17 18:22:41 +0200

    finish new tips page

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 // ============================= LOGIN POPUP =========================
-document.querySelector("#login-button").addEventListener("click", function () {
-  document.querySelector(".popout").classList.add("active");
-});
-document
-  .querySelector(".popout .close-btn")
-  .addEventListener("click", function () {
-    document.querySelector(".popout").classList.remove("active");
-  });
+// document.querySelector("#login-button").addEventListener("click", function () {
+//   document.querySelector(".popout").classList.add("active");
+// });
+// document
+//   .querySelector(".popout .close-btn")
+//   .addEventListener("click", function () {
+//     document.querySelector(".popout").classList.remove("active");
+//   });
 // ============================= ACCORDION =========================
-document.querySelector(".title").addEventListener("click", function () {
-  document.querySelector(".acc-content").classList.add("active");
-  document.querySelector(".title").classList.add("active");
+const boxes = document.querySelectorAll(".box");
+boxes.forEach((box) => {
+  box.addEventListener("click", () => {
+    box.classList.toggle("active");
+  });
 });
